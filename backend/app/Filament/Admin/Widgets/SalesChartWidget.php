1393e0ae81366f8ac9a58b572bfd787e535522a6
LoDRZin <roger.derik211@gmail.com>
2026-07-17 00:25:15 -0300

    style(dashboard): diminui altura maxima do grafico de vendas

--- backend/app/Filament/Admin/Widgets/SalesChartWidget.php
+++ backend/app/Filament/Admin/Widgets/SalesChartWidget.php
@@ -16,6 +16,7 @@ class SalesChartWidget extends ChartWidget
     protected ?string $heading = 'Receita e Vendas';
     protected static ?int $sort = 1;
     protected int | string | array $columnSpan = 'full';
+    protected static ?string $maxHeight = '275px';
 
     protected function getData(): array
     {
