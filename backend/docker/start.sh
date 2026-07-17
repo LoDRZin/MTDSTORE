76ddf50e4fede7d6ee241bc82060034151b4214b
LoDRZin <roger.derik211@gmail.com>
2026-07-16 22:44:21 -0300

    chore(infra): substitui artisan serve por Nginx + PHP-FPM + OPcache no Dockerfile

---
+++ backend/docker/start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Injetar a variável $PORT do Render na configuração do Nginx
+mkdir -p /etc/nginx/conf.d
+envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+rm -f /etc/nginx/sites-enabled/default
+
+# Garantir permissões corretas
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+
+# Otimizar a aplicação Laravel (Caching)
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan event:cache
+
+# Iniciar o Supervisor (que irá gerenciar Nginx, PHP-FPM, Horizon, etc.)
+exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
