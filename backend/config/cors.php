628a7e127fdd277a2377ad4aa9e9fe6cc99d8d33
LoDRZin <roger.derik211@gmail.com>
2026-07-17 21:09:52 -0300

    fix: allow www subdomain in backend CORS config

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'https://mtdstore.xyz',
+        'https://www.mtdstore.xyz'
+    ],
 
     'allowed_origins_patterns' => [],
 
